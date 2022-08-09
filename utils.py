ad49306898e9228f9531f596ff613f39ade20d99
guaguakai <kaiwang@g.harvard.edu>
2022-08-09 02:33:25 -0400

    added script to screenshot and save the figures to local folder

--- utils.py
+++ utils.py
@@ -6,7 +6,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def screenShot(url, filename):
+# source id list
+source_id_dict = {
+    2014: 10,
+    2017: 31026,
+    2019: 11351,
+    2020: 9181,
+    2021: 15423,
+    }
+
+def screenShot(location_id, lat, lon, year):
+    lat_diff = 0.00689
+    lon_diff = 0.01169
+    folder_path = 'data/images/'
+
+    source_id = source_id_dict[year]
+    lon_min, lon_max = lon - lon_diff/2, lon + lon_diff/2
+    lat_min, lat_max = lat - lat_diff/2, lat + lat_diff/2
+
+    url = 'https://livingatlas.arcgis.com/wayback/#active={}&ext={},{},{},{}&localChangesOnly=true'.format(source_id, lon_min, lat_min, lon_max, lat_max)
+    print(url)
+    filename = 'location{}_year{}.png'.format(location_id, year)
+
     driver = webdriver.Chrome(executable_path='drivers/linux/chromedriver') # Linux
     # driver = webdriver.Chrome(executable_path='drivers/macos/chromedriver') # Mac
 
@@ -21,27 +42,13 @@ def screenShot(url, filename):
     child.click()
     sleep(3)
 
-    driver.get_screenshot_as_file(filename)
+    driver.get_screenshot_as_file(folder_path + filename)
     driver.quit()
-
-
-# source id list
-source_id_dict = {
-    2014: 10,
-    2017: 31026,
-    2019: 11351,
-    2020: 9181,
-    2021: 15423,
-    }
 
 if __name__ == '__main__':
     lat, lon = 23.94087, 120.64671
-    lat_diff = 0.00689
-    lon_diff = 0.01169
     year = 2020 # available years: 2014, 2017, 2019, 2020, 2021
 
     source_id = source_id_dict[year]
-    url = 'https://livingatlas.arcgis.com/wayback/#active={}&ext={},{},{},{}&localChangesOnly=true'.format(source_id, lon, lat, lon+lon_diff, lat+lat_diff)
-    filename = 'screenshot.png'
-    screenShot(url, filename)
+    screenShot(0, lat, lon, year)
 
